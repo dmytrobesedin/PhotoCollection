e9617859601155ebbd4b21eab04afb963c87d618
dmytrobesedin <dbesedin7@gmail.com>
2019-08-22 23:33:26 +0300

    Initial Commit

---
+++ PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Дмитрий Беседин on 8/22/19.
+//  Copyright © 2019 DmytroBesedin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
